cf233b0ba86c178bf5477ebde7f6893f226f04c4
oleksandra <104913146+leksi-s@users.noreply.github.com>
2022-05-05 13:46:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿var people = new List<Person>
+{
+    new Person("Maria", 11789),
+    new Person("Anna", 12345),
+    new Person("Irina", 50888),
+    new Person("Kateryna", 55555),
+};
+
+foreach (var p in people)
+    Console.WriteLine($"{p.Name} - {p.Id}");
+
+Console.WriteLine("\n\nsorting by name.");
+
+var sortedPeople1 = from p in people orderby p.Name select p;
+
+foreach (var p in sortedPeople1)
+    Console.WriteLine($"{p.Name} - {p.Id}");
+
+Console.WriteLine("\n\nsorting by id.");
+
+var sortedPeople2 = from p in people orderby p.Id select p;
+
+foreach (var p in sortedPeople2)
+    Console.WriteLine($"{p.Name} - {p.Id}");
+
+
+
+
+class Person 
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    public Person(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+}    
